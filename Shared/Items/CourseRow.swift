c0764f9a43b72138d46d637027e9145ff5d4e9fc
RyDev <ryanjnewsom@gmail.com>
2021-02-06 14:58:14 -0800

    end of 14th video, passing namespace

--- Shared/Items/CourseRow.swift
+++ Shared/Items/CourseRow.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 
 struct CourseRow: View {
+    var item: CourseSection = courseSections[0]
+    
     var body: some View {
         
         HStack(alignment: .top) {
-            Image(systemName: "paperplane.circle.fill")
-                .renderingMode(.template)
+            Image(item.logo)
+                .renderingMode(.original)
                 .frame(width: 48.0, height: 48.0)
                 .imageScale(.large)
-                .background(Color.blue)
+                .background(item.color)
                 .clipShape(Circle())
-                .foregroundColor(.white)
             VStack(alignment: .leading, spacing: 4.0) {
-                Text("Swift UI")
+                Text(item.title)
                     .font(.subheadline)
                     .bold()
-                Text("Description")
+                Text(item.subtitle)
                     .font(.footnote)
                     .foregroundColor(.secondary)
             }
