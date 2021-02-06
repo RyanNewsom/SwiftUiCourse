c9eb323264cec958c4b5a4318bfd9ec3ad1a35b3
RyDev <ryanjnewsom@gmail.com>
2021-02-06 11:26:51 -0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Ryan Newsom on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
