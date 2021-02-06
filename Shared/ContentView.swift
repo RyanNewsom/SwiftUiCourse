0dec611fad60e01c8f99182a999b81f2ec47f4d4
RyDev <ryanjnewsom@gmail.com>
2021-02-06 11:57:40 -0800

    end of 2nd video

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+
+            Text("Swift UI for iOS 14")
+                .font(.body)
+                .fontWeight(.bold)
+                
+            Text("20 Sections")
+                .font(.footnote)
+                .fontWeight(.bold)
+                
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: 200.0, height: 200.0       ))
+                .preferredColorScheme(.dark)
+        }
     }
 }
